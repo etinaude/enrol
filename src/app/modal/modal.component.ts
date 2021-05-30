b8af03412d198fc5eb030bd38f7b43454182edb9
Etienne <etinaude@gmail.com>
2021-05-30 23:43:08 +1200

    modal upheaval

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+export interface courseType {
+  title: string;
+  subTitle: string;
+  points: number;
+  description: string;
+  stars: number[];
+  required: boolean;
+  prereq: string;
+}
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
 })
 export class ModalComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(@Inject(MAT_DIALOG_DATA) public course: courseType) {
+    console.log(course);
   }
 
+  ngOnInit(): void {}
 }
